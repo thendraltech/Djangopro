f63fa299ede2ae51bb64441fdd7aaaaa67051063
bootstrap <thendralstech@gmail.com>
2023-01-13 02:32:49 +0530

    first commit

---
+++ mysite/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+def products(request):
+    products = Product.objects.all()
+    context={
+        'products':products
+    }
+    return render(request, 'myapp/index.html',context)
+
+def product_detail(request,id):
+    product = Product.objects.get(id=id)
+    context={
+        'product':product
+    }
+    return render(request,'myapp/detail.html',context)
+    
+    
